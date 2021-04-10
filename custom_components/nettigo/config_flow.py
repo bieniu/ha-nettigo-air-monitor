5fafeccd424d4af24a772082b4dc2f801120eb9e
Maciej Bieniek <maciej.bieniek@gmail.com>
2021-04-10 12:27:41 +0200

    Clean config flow

--- custom_components/nettigo/config_flow.py
+++ custom_components/nettigo/config_flow.py
@@ -1,6 +1,4 @@
 """Adds config flow for Nettigo."""
-import ipaddress
-import re
 import logging
 from typing import Optional
 
@@ -9,23 +7,15 @@ import async_timeout
 from nettigo import ApiError, CannotGetMac, Nettigo
 import voluptuous as vol
 
-from homeassistant import config_entries, exceptions
+from homeassistant import config_entries
 from homeassistant.const import CONF_HOST
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.device_registry import format_mac
+from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 
 from .const import DOMAIN  # pylint:disable=unused-import
 
-
 _LOGGER = logging.getLogger(__name__)
-
-def host_valid(host: str) -> bool:
-    """Return True if hostname or IP address is valid."""
-    try:
-        return ipaddress.ip_address(host).version == (4 or 6)
-    except ValueError:
-        disallowed = re.compile(r"[^a-zA-Z\d\-]")
-        return all(x and not disallowed.search(x) for x in host.split("."))
 
 
 class NettigoFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
@@ -35,24 +25,14 @@ class NettigoFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
     CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_POLL
     host = None
 
-    async def async_step_user(self, user_input: Optional[dict] = None) -> dict:
+    async def async_step_user(self, user_input: Optional[ConfigType] = None) -> dict:
         """Handle a flow initialized by the user."""
         errors = {}
-
-        websession = async_get_clientsession(self.hass)
 
         if user_input is not None:
             host = user_input[CONF_HOST]
             try:
-                if not host_valid(host):
-                    raise InvalidHost()
-
-                nettigo = Nettigo(websession, host)
-
-                with async_timeout.timeout(5):
-                    mac = await nettigo.async_get_mac_address()
-            except InvalidHost:
-                errors["base"] = "invalid_host"
+                mac = await self._async_get_mac(host)
             except (ApiError, ClientConnectorError):
                 errors["base"] = "cannot_connect"
             except CannotGetMac:
@@ -80,15 +60,12 @@ class NettigoFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
             errors=errors,
         )
 
-    async def async_step_zeroconf(self, zeroconf_info: dict) -> dict:
+    async def async_step_zeroconf(self, zeroconf_info: DiscoveryInfoType):
         """Handle zeroconf discovery."""
         self.host = zeroconf_info[CONF_HOST]
-        websession = async_get_clientsession(self.hass)
 
         try:
-            nettigo = Nettigo(websession, self.host)
-            with async_timeout.timeout(5):
-                mac = await nettigo.async_get_mac_address()
+            mac = await self._async_get_mac(self.host)
         except (ApiError, ClientConnectorError):
             return self.async_abort(reason="cannot_connect")
         except CannotGetMac:
@@ -103,9 +80,7 @@ class NettigoFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
 
         return await self.async_step_confirm_discovery()
 
-    async def async_step_confirm_discovery(
-        self, user_input: Optional[dict] = None
-    ) -> dict:
+    async def async_step_confirm_discovery(self, user_input=None):
         """Handle discovery confirm."""
         errors = {}
 
@@ -123,6 +98,9 @@ class NettigoFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
             errors=errors,
         )
 
-
-class InvalidHost(exceptions.HomeAssistantError):
-    """Error to indicate that hostname/IP address is invalid."""
+    async def _async_get_mac(self, host: str) -> str:
+        """Get device MAC address."""
+        websession = async_get_clientsession(self.hass)
+        nettigo = Nettigo(websession, host)
+        with async_timeout.timeout(5):
+            return await nettigo.async_get_mac_address()
